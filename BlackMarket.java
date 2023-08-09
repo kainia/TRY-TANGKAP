48c52f4c026203aa925334c249c19dc6510e22e1
kaizer <141194089+kainia@users.noreply.github.com>
2023-08-09 08:19:16 +0800

    Add files via upload

---
+++ BlackMarket.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blackmarket;
+
+/**
+ *
+ * @author User
+ */
+public class BlackMarket {
+
+    public static void main(String[] args) {
+        try{
+            String[] BlackMarket = {"Ketum","Syabu Naga","Heroin","Maria","Glock","AK-47","AWM","KLEE","KAMEN RIDER","WONDER PET"};
+            System.out.println(BlackMarket[4]);
+        }
+        catch (Exception ex){
+            System.out.println("STOCK HABIH");
+        }
+    }
+}
