d4bd9787fc1a7db0823a1c1b5bd5a9895e1afb2d
kaizer <141194089+kainia@users.noreply.github.com>
2023-08-23 07:52:55 +0800

    Add files via upload

---
+++ Nom3.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nom3;
+
+/**
+ *
+ * @author User
+ */
+
+class Animal {
+  String name;
+
+  public Animal(String name) {
+    this.name = name;
+  }
+
+  public void speak() {
+    System.out.println("AKU KUCING.");
+  }
+
+  public void introduce() {
+    System.out.println("WASSSUP AKU " + name + ".");
+  }
+}
+
+class Cat extends Animal {
+
+  public Cat(String name) {
+    super(name);
+  }
+
+  public void meow() {
+    System.out.println("RAWRRRRRRRS!");
+  }
+}
+
+public class Nom3 {
+  public static void main(String[] args) {
+    Cat cat = new Cat("OYEN");
+    cat.introduce(); // Calls the introduce() method from the Animal class
+    cat.meow();
+  }
+}
